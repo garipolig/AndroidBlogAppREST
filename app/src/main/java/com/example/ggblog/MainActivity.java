80b59a7586f3ac36d7ce1623672849ca98bd597f
Giuseppe Garipoli <garipolig@tiscali.it>
2019-06-09 01:12:40 +0200

    Retrieving the list of authors from Web Server
    Google Volley library is used to communicate
    with the Web Server through REST APIs

--- app/src/main/java/com/example/ggblog/MainActivity.java
+++ app/src/main/java/com/example/ggblog/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -14,9 +15,29 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainBlogPage";
+
+    private static final String GET_AUTHORS_URL =
+            "http://sym-json-server.herokuapp.com/authors";
+
+    private static final String GET_FIRST_PAGE = GET_AUTHORS_URL + "?_page=1";
+    // Possible extension: make those parameters user configurable through UI Settings
+    private static final String AUTHORS_SORTING_ATTRIBUTES = "name";
+    private static final String AUTHORS_ORDERING_METHODS = "asc";
+    private static final String MAX_NUM_AUTHORS_PER_PAGE = "20";
 
     private TextView mHeaderTextView;
     private ListView mAuthorsListView;
@@ -68,6 +89,11 @@ public class MainActivity extends AppCompatActivity {
                 // TODO
             }
         });
+        retrieveAuthorsList(
+                GET_FIRST_PAGE +
+                "&_sort=" + AUTHORS_SORTING_ATTRIBUTES +
+                "&_order=" + AUTHORS_ORDERING_METHODS +
+                "&_limit=" + MAX_NUM_AUTHORS_PER_PAGE);
     }
 
     @Override
@@ -89,5 +115,37 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void retrieveAuthorsList(String url) {
+        RequestQueue queue = Volley.newRequestQueue(this);
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Log.i(TAG, "Response: " + response.toString());
+                        for (int i = 0; i < response.length(); i++) {
+                            try {
+                                JSONObject jsonObject = response.getJSONObject(i);
+                                String currId = jsonObject.getString("id");
+                                Log.i(TAG, "Current Author Id: " + currId);
+                                String currName = jsonObject.getString("name");
+                                Log.i(TAG, "Current Author Name: " + currName);
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e(TAG, "ERROR");
+                    }
+                });
+
+        // Add the request to the RequestQueue
+        //jsonArrayRequest.setShouldCache(false);
+        jsonArrayRequest.setTag(TAG);
+        queue.add(jsonArrayRequest);
     }
 }
