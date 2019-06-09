b9004bb32f12a179adc1cf0529e2663e0b7aca4f
Giuseppe Garipoli <garipolig@tiscali.it>
2019-06-09 09:55:07 +0200

    Custom JsonArrayRequest to retrieve Header Link
    The default implementation of JsonArrayRequest is not
    providing the Header. Extending it to be able to
    retrieve the "Link" section, which is providing info
    about the URL request to use for moving to first, prev,
    next and last page. This is needed because I'm using
    pagination when requesting the author list.

--- app/src/main/java/com/example/ggblog/MainActivity.java
+++ app/src/main/java/com/example/ggblog/MainActivity.java
@@ -11,24 +11,44 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.HttpHeaderParser;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.Volley;
+import com.android.volley.NetworkResponse;
+import com.android.volley.ParseError;
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+// TODO: add DBG, VDBG macros and optimize logging
+// TODO: Cancel ongoing request to new Authors page if user clicks on a specific Author
+
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainBlogPage";
+
+    private static final String FIRST_PAGE = "first";
+    private static final String PREV_PAGE = "prev";
+    private static final String NEXT_PAGE = "next";
+    private static final String LAST_PAGE = "last";
+
+    private static final String PAGE_LINK_REGEXP = "<([^\"]*)>";
+    private static final String PAGE_REL_REGEXP = "\"([^\"]*)\"";
 
     private static final String GET_AUTHORS_URL =
             "http://sym-json-server.herokuapp.com/authors";
@@ -45,6 +65,78 @@ public class MainActivity extends AppCompatActivity {
     private Button mButtonPrevPage;
     private Button mButtonNextPage;
     private Button mButtonLastPage;
+
+    private String mCurrentPageRequest;
+    private String mFirstPageRequest;
+    private String mPrevPageRequest;
+    private String mNextPageRequest;
+    private String mLastPageRequest;
+
+    // Needing a Custom JsonArrayRequest, to retrieve the Link from header, which is not
+    // retrieved by the default implementation
+    // Since we are using pagination when retrieving the authors, the Link is needed to retrieve
+    // the URL Requests to be used to move from the current page to the first/previous/next/last.
+    public class CustomJsonArrayRequest extends JsonArrayRequest {
+        private String mRequestedUrl;
+
+        public CustomJsonArrayRequest(String url, Response.Listener
+                <JSONArray> listener, Response.ErrorListener errorListener) {
+            super(url, listener, errorListener);
+            mRequestedUrl = url;
+        }
+
+        public CustomJsonArrayRequest(int method, String url, JSONArray jsonRequest, Response.Listener
+                <JSONArray> listener, Response.ErrorListener errorListener) {
+            super(method, url, jsonRequest, listener, errorListener);
+            mRequestedUrl = url;
+        }
+
+        @Override
+        protected Response<JSONArray> parseNetworkResponse(NetworkResponse response) {
+            try {
+                String jsonStringData = new String(response.data,
+                        HttpHeaderParser.parseCharset(response.headers, PROTOCOL_CHARSET));
+                Log.i(TAG, "Full Header: " + response.headers);
+                String jsonStringHeaderLink = response.headers.get("Link");
+                Log.i(TAG, "Header Link: " + jsonStringHeaderLink);
+                mCurrentPageRequest = mRequestedUrl;
+                // Resetting the page requests that are available on the current page displayed
+                // Those URL requests will be used by specific buttons, to move between pages
+                mFirstPageRequest = null;
+                mPrevPageRequest = null;
+                mNextPageRequest = null;
+                mLastPageRequest = null;
+                //  Page link is placed between <>
+                Pattern patternPageLink = Pattern.compile(PAGE_LINK_REGEXP);
+                Matcher matcherPageLink = patternPageLink.matcher(jsonStringHeaderLink);
+                //  Page rel (first, next, prev, last) is placed around ""
+                Pattern patternPageRel = Pattern.compile(PAGE_REL_REGEXP);
+                Matcher matcherPageRel = patternPageRel.matcher(jsonStringHeaderLink);
+                while(matcherPageLink.find()) {
+                    String currPageLink = matcherPageLink.group(1);
+                    Log.i(TAG, "PageLink: " + currPageLink);
+                    matcherPageRel.find();
+                    String currPageRel = matcherPageRel.group(1);
+                    Log.i(TAG, "PageRel: " + currPageRel);
+                    if (currPageRel.equals(FIRST_PAGE)) {
+                        mFirstPageRequest = currPageLink;
+                    } else if (currPageRel.equals(PREV_PAGE)) {
+                        mPrevPageRequest = currPageLink;
+                    } else if (currPageRel.equals(NEXT_PAGE)) {
+                        mNextPageRequest = currPageLink;
+                    }  else if (currPageRel.equals(LAST_PAGE)) {
+                        mLastPageRequest = currPageLink;
+                    }
+                }
+                return Response.success(
+                        new JSONArray(jsonStringData), HttpHeaderParser.parseCacheHeaders(response));
+            } catch (UnsupportedEncodingException e) {
+                return Response.error(new ParseError(e));
+            } catch (JSONException je) {
+                return Response.error(new ParseError(je));
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,28 +157,36 @@ public class MainActivity extends AppCompatActivity {
         mButtonFirstPage.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                // TODO
+                if (mFirstPageRequest != null) {
+                    retrieveAuthorsList(mFirstPageRequest);
+                }
             }
         });
         mButtonPrevPage = (Button) findViewById(R.id.buttonPrevPage);
         mButtonPrevPage.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                // TODO
+                if (mPrevPageRequest != null) {
+                    retrieveAuthorsList(mPrevPageRequest);
+                }
             }
         });
         mButtonNextPage = (Button) findViewById(R.id.buttonNextPage);
         mButtonNextPage.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                // TODO
+                if (mNextPageRequest != null) {
+                    retrieveAuthorsList(mNextPageRequest);
+                }
             }
         });
         mButtonLastPage = (Button) findViewById(R.id.buttonLastPage);
         mButtonLastPage.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                // TODO
+                if (mLastPageRequest != null) {
+                    retrieveAuthorsList(mLastPageRequest);
+                }
             }
         });
         retrieveAuthorsList(
@@ -119,33 +219,59 @@ public class MainActivity extends AppCompatActivity {
 
     private void retrieveAuthorsList(String url) {
         RequestQueue queue = Volley.newRequestQueue(this);
-        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest
+        CustomJsonArrayRequest jsonArrayRequest = new CustomJsonArrayRequest
                 (Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
-                    @Override
-                    public void onResponse(JSONArray response) {
-                        Log.i(TAG, "Response: " + response.toString());
-                        for (int i = 0; i < response.length(); i++) {
-                            try {
-                                JSONObject jsonObject = response.getJSONObject(i);
-                                String currId = jsonObject.getString("id");
-                                Log.i(TAG, "Current Author Id: " + currId);
-                                String currName = jsonObject.getString("name");
-                                Log.i(TAG, "Current Author Name: " + currName);
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                            }
-                        }
+            @Override
+            public void onResponse(JSONArray response) {
+                Log.i(TAG, "Response: " + response.toString());
+                // We will only visualize the Author names
+                ArrayList<String> authorsList = new ArrayList<String>();
+                for (int i = 0; i < response.length(); i++) {
+                    try {
+                        JSONObject jsonObject = response.getJSONObject(i);
+                        String currId = jsonObject.getString("id");
+                        Log.i(TAG, "Current Author Id: " + currId);
+                        String currName = jsonObject.getString("name");
+                        Log.i(TAG, "Current Author Name: " + currName);
+                        authorsList.add(currName);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
                     }
-                }, new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Log.e(TAG, "ERROR");
-                    }
-                });
-
+                }
+                updateAuthorListView(authorsList);
+                updateAvailableButtons();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(TAG, "ERROR");
+            }
+        });
         // Add the request to the RequestQueue
         //jsonArrayRequest.setShouldCache(false);
         jsonArrayRequest.setTag(TAG);
         queue.add(jsonArrayRequest);
+    }
+
+    private void updateAuthorListView(ArrayList<String> authorsList) {
+        ArrayAdapter<String> authorsListAdapter =
+                new ArrayAdapter<String>(getApplicationContext(), R.layout.simple_row, authorsList);
+        mAuthorsListView.setAdapter(authorsListAdapter);
+    }
+
+    private void updateAvailableButtons() {
+        // Enable/Disable the buttons according to the available page request
+        Log.i(TAG, "First Page Request=" + mFirstPageRequest);
+        // Avoiding to display the First Page Button if we are already at the First Page
+        mButtonFirstPage.setEnabled(mFirstPageRequest != null &&
+                !mCurrentPageRequest.equals(mFirstPageRequest));
+        Log.i(TAG, "Prev Page Request=" + mPrevPageRequest);
+        mButtonPrevPage.setEnabled(mPrevPageRequest != null);
+        Log.i(TAG, "Next Page Request=" + mNextPageRequest);
+        mButtonNextPage.setEnabled(mNextPageRequest != null);
+        Log.i(TAG, "Last Page Request=" + mLastPageRequest);
+        // Avoiding to display the Last Page Button if we are already at the Last Page
+        mButtonLastPage.setEnabled(mLastPageRequest != null &&
+                !mCurrentPageRequest.equals(mLastPageRequest));
     }
 }
